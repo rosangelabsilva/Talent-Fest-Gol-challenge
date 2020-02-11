6a39e9845d64a5b6abd39b5ed56505646b8b6889
rosangelabsilva <rosangela-bsilva@hotmail.com>
2020-02-11 12:13:13 -0300

    configuração firebase

---
+++ src/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5RU-p13vPu5kE49Ayln5OkME06qmwfV4",
+    authDomain: "gol-talent.firebaseapp.com",
+    databaseURL: "https://gol-talent.firebaseio.com",
+    projectId: "gol-talent",
+    storageBucket: "gol-talent.appspot.com",
+    messagingSenderId: "152501459959",
+    appId: "1:152501459959:web:de2294b9f462a2141eecf2",
+    measurementId: "G-SDH23MQDV0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
