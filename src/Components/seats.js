882d15ad01d4bbb2a8c0d3d3c055ff7478cd00a6
Jessica Patricia <jessicapatricia03@hotmail.com>
2020-02-11 18:49:56 -0300

    header home

---
+++ src/Components/seats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Seats (props){
+    return(
+        <img className={props.className}
+             onClick={props.onClick}
+             disabled={props}
+             id={props.id}
+        >
+        </img>
+    )
+}
+export default Seats;
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { StyleSheet, css } from 'aphrodite';
+import exit from './img/logout.png'
+
 
 const Home = () => {
 
     return(
-        <main className="home">
-            Home
-        </main>
+        <>
+            <header className={css(styles.headerHome)}>
+                <p>Olá <span>Alessandra,</span></p>
+                <img  src={exit } />
+            </header>
+            <main className="home">
+                Home
+            </main>
+        </>
     )
-}
+};
 
+const styles = StyleSheet.create({
+    headerHome: {
+        backgroundColor: 'red',
+    }
+})
 
 export default Home;
