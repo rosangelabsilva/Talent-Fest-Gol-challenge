0fd365ee2d0a7167de772738dabcdaf6885ecec5
erickalemos <ericka.florentino@gmail.com>
2020-02-11 15:46:59 -0300

    criação de componentes

---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Input (props){
+    return(
+        <input className={props.className} 
+        type={props.type} 
+        placeholder={props.placeholder}>
+        </input>
+    )
+}
+
+export default Input;
