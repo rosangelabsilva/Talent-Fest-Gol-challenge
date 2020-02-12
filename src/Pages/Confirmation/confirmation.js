a274ee9f4a88216565993e500a2d2c73c2edfe9f
Joanilza <jbockhorny@gmail.com>
2020-02-12 12:24:20 -0300

    confirmation

--- src/Pages/Confirmation/confirmation.js
+++ src/Pages/Confirmation/confirmation.js
@@ -2,38 +2,61 @@ import React from "react";
 import Head from "../../Components/header";
 import Footer from "../../Components/footer";
 import { StyleSheet, css } from 'aphrodite';
+import Icon from './img/check.png';
 
 const style = StyleSheet.create({
+
+    body: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',   
+        height: '100vh',     
+    },
+
     main: {
         
         display: 'flex',
         flexDirection: 'column',
-        width: '100%',
+        width: '97%',
         height: '50%',
         border: '1px solid #000000',
         padding: '1%',
-        marginTop: '20%',
-        marginBottom: '20%',
+        flexDirection: 'column',
+        justifyContent: 'space-between',          
+        // marginTop: '10%',
+        // marginBottom: '10%',
     },
     background: {
+        display: 'flex',
+        flexDirection: 'column',
         backgroundColor: '#ff5a00',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignContent: 'center', 
-        
+        width: '100%',
+        height: '100%',
+        fontSize: '1rem',
+        background: '#FF5A00',
+        color: '#FFFFFF',
+               
     }, 
     teste: {
-        padding: '10%',
+        padding: '5%', 
 
+    },
+    teste2: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
     },
 
     label: {
         justifyContent: 'center',
         alignContent: 'center',   
-        padding: '2%', 
+        padding: '1%', 
+
     },
     icon: {
-
-
+        marginTop: '10%',
     }
 })
 
@@ -41,7 +64,7 @@ const style = StyleSheet.create({
 const Confirmation = () => {
 
     return(
-        <>
+        < div className={css(style.body)}>
         <Head 
         title ='Confirmação'/>
         <main className={css(style.main)}>
@@ -52,23 +75,25 @@ const Confirmation = () => {
             <p classNAme={css(style.label)}>IDA - Voo NUMERO</p>
             <p classNAme={css(style.label)}>Número da confirmação: NUMERO</p>
             <p classNAme={css(style.label)}>Cliente: CLIENTE</p>
-            <p classNAme={css(style.label)}>Assento reservado: ASSENTO</p>
-
             
+            <p classNAme={css(style.label)}>Assento reservado: ASSENTO</p>
+            
+
+            <div className={css(style.teste2)}>
 
             <img 
                 className={css(style.icon)} 
-                src="./check.png" 
+                src={Icon} 
                 alt="check"
             />
+            </div>
             </div>
             </div>
 
         </main>
 
-        <Footer 
-        primarycontent ='PROSSEGUIR'/>
-        </>         
+        <Footer />
+        </div>       
 
     )
 }
