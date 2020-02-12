17f85d13a315fe0844b15f4420fef3e440b32696
Joanilza <jbockhorny@gmail.com>
2020-02-12 10:57:54 -0300

    confirmation

--- src/Pages/Confirmation/confirmation.js
+++ src/Pages/Confirmation/confirmation.js
@@ -1,11 +1,75 @@
 import React from "react";
+import Head from "../../Components/header";
+import Footer from "../../Components/footer";
+import { StyleSheet, css } from 'aphrodite';
+
+const style = StyleSheet.create({
+    main: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '50%',
+        border: '1px solid #000000',
+        padding: '1%',
+        marginTop: '20%',
+        marginBottom: '20%',
+    },
+    background: {
+        backgroundColor: '#ff5a00',
+        justifyContent: 'center',
+        alignContent: 'center', 
+        
+    }, 
+    teste: {
+        padding: '10%',
+
+    },
+
+    label: {
+        justifyContent: 'center',
+        alignContent: 'center',   
+        padding: '2%', 
+    },
+    icon: {
+
+
+    }
+})
+
 
 const Confirmation = () => {
 
     return(
-        <main className="confirmation">
-            Confirmacoes
+        <>
+        <Head 
+        title ='Confirmação'/>
+        <main className={css(style.main)}>
+            <div className={css(style.background)}>
+                <div className={css(style.teste)}>
+
+            <p classNAme={css(style.label)}>Reserva de assento finalizado!</p>
+            <p classNAme={css(style.label)}>IDA - Voo NUMERO</p>
+            <p classNAme={css(style.label)}>Número da confirmação: NUMERO</p>
+            <p classNAme={css(style.label)}>Cliente: CLIENTE</p>
+            <p classNAme={css(style.label)}>Assento reservado: ASSENTO</p>
+
+            
+
+            <img 
+                className={css(style.icon)} 
+                src="./check.png" 
+                alt="check"
+            />
+            </div>
+            </div>
+
         </main>
+
+        <Footer 
+        primarycontent ='PROSSEGUIR'/>
+        </>         
+
     )
 }
 
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, css } from 'aphrodite';
 import exit from './img/logout.png';
 import firebase from 'firebase';
@@ -6,119 +6,119 @@ import Header from '../../Components/header';
 import { db } from '../../config';
 
 const Home = () => {
-    const [userName, setUsername] = useState('');
-    const user = firebase.auth().currentUser;
-    useEffect( () => {
-        firebase
-        .auth()
-        .onAuthStateChanged( (user) => {
-          if(user){
-            db.collection('clients')
+  const [userName, setUsername] = useState('');
+  const user = firebase.auth().currentUser;
+  useEffect(() => {
+    firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        if (user) {
+          db.collection('clients')
             .doc(user.uid)
             .get()
-            .then( user => {
-                setUsername(user.data().nome);
+            .then(user => {
+              setUsername(user.data().nome);
             });
-          }
-        })
-      }, [])  
+        }
+      })
+  }, [])
 
-    return(
-        <>
-            <Header 
-                title = {"Bem vinda(o)" + userName}
-            />
-                <img  src={exit } />
-            <main className="home">
-                <div className={css(styles.locator)}>
-                    <p>LOCALIZADOR: <span className={css(styles.code)}>GNRHYZ</span></p>
-                    <p className={css(styles.status)}>IDA - voo G3 1265</p>
-                    <div className={css(styles.passageDetail)}>
-                        <ul>
-                            <li className={css(styles.lists)}>Guarulhos(GRU)</li>
-                            <li className={css(styles.lists)}>São Paulo</li>
-                            <li className={css(styles.lists)}>15/02/2020 -10h00</li>
-                            <li className={css(styles.lists)}>Voo Direto</li>
-                        </ul>
-                        <ul>
-                            <li className={css(styles.lists)}>Cuiaba (CGB)</li>
-                            <li className={css(styles.lists)}>Mato Grosso</li>
-                            <li className={css(styles.lists)}> 15/02/2020 -11h20</li>
-                            <li className={css(styles.lists)}>Check-in ainda não realizado</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className={css(styles.locator)}>
-                    <p>LOCALIZADOR: <span className={css(styles.code)}>GAFHFI</span></p>
-                    <p className={css(styles.status)}>VOLTA - Voo G7 1895</p>
-                    <div className={css(styles.passageDetail)}>
-                        <ul>
-                            <li className={css(styles.lists)}>Cuiabá (CGB)</li>
-                            <li className={css(styles.lists)}>Mato Grosso</li>
-                            <li className={css(styles.lists)}>21/02/2020 - 15h00</li>
-                            <li className={css(styles.lists)}>Voo Direto</li>
-                        </ul>
-                        <ul>
-                            <li className={css(styles.lists)}>Guarulhos (CGB)</li>
-                            <li className={css(styles.lists)}>São Paulo</li>
-                            <li className={css(styles.lists)}>21/02/2020 - 16h20</li>
-                            <li className={css(styles.lists)}>Check-in ainda não realizado</li>
-                        </ul>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Header
+        title={"Bem vinda(o)" + userName}
+      />
+      <img src={exit} />
+      <main className="home">
+        <div className={css(styles.locator)}>
+          <p>LOCALIZADOR: <span className={css(styles.code)}>GNRHYZ</span></p>
+          <p className={css(styles.status)}>IDA - voo G3 1265</p>
+          <div className={css(styles.passageDetail)}>
+            <ul>
+              <li className={css(styles.lists)}>Guarulhos(GRU)</li>
+              <li className={css(styles.lists)}>São Paulo</li>
+              <li className={css(styles.lists)}>15/02/2020 -10h00</li>
+              <li className={css(styles.lists)}>Voo Direto</li>
+            </ul>
+            <ul>
+              <li className={css(styles.lists)}>Cuiaba (CGB)</li>
+              <li className={css(styles.lists)}>Mato Grosso</li>
+              <li className={css(styles.lists)}> 15/02/2020 -11h20</li>
+              <li className={css(styles.lists)}>Check-in ainda não realizado</li>
+            </ul>
+          </div>
+        </div>
+        <div className={css(styles.locator)}>
+          <p>LOCALIZADOR: <span className={css(styles.code)}>GAFHFI</span></p>
+          <p className={css(styles.status)}>VOLTA - Voo G7 1895</p>
+          <div className={css(styles.passageDetail)}>
+            <ul>
+              <li className={css(styles.lists)}>Cuiabá (CGB)</li>
+              <li className={css(styles.lists)}>Mato Grosso</li>
+              <li className={css(styles.lists)}>21/02/2020 - 15h00</li>
+              <li className={css(styles.lists)}>Voo Direto</li>
+            </ul>
+            <ul>
+              <li className={css(styles.lists)}>Guarulhos (CGB)</li>
+              <li className={css(styles.lists)}>São Paulo</li>
+              <li className={css(styles.lists)}>21/02/2020 - 16h20</li>
+              <li className={css(styles.lists)}>Check-in ainda não realizado</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 };
 
 const styles = StyleSheet.create({
-    headerHome: {
-        backgroundColor: 'red',
-        display: 'flex',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-    },
+  headerHome: {
+    backgroundColor: 'red',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
 
-    user: {
-        color: '#FFFFFF',
-        fontSize: '20px',
-    },
+  user: {
+    color: '#FFFFFF',
+    fontSize: '20px',
+  },
 
-    exit: {
-        width: '10%',
-    },
+  exit: {
+    width: '10%',
+  },
 
-    locator: {
-        border: '1px solid #000000',
-        margin: '0.3rem',
-        fontSize: '1.3rem',
-    },
+  locator: {
+    border: '1px solid #000000',
+    margin: '0.3rem',
+    fontSize: '1.3rem',
+  },
 
-    passageDetail: {
-        display: 'flex',
-        justifyContent: 'space-around',
-        alignItems: 'center',
+  passageDetail: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
 
-    },
+  },
 
-    status: {
-        background: '#FF5A00',
-        height: '2rem',
-        display: 'flex',
-        alignItems: 'center',
-        paddingLeft: '1rem',
-        fontSize: '1.3rem',
-        color: '#FFFFFF',
-    },
+  status: {
+    background: '#FF5A00',
+    height: '2rem',
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: '1rem',
+    fontSize: '1.3rem',
+    color: '#FFFFFF',
+  },
 
-    lists: {
-        listStyleType: 'none',
-        fontSize: '1rem',
-    },
+  lists: {
+    listStyleType: 'none',
+    fontSize: '1rem',
+  },
 
-    code: {
-        fontSize: '1.3rem',
-    }
+  code: {
+    fontSize: '1.3rem',
+  }
 })
 
 export default Home;
